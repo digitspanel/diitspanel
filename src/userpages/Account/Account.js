1526525ea8c5a1156b0aa1b41824228033ae04ad
Asif <Farhan.Asif.consultant@nielsen.com>
2020-09-24 20:02:07 +0500

    Fixed Some Bugs

--- src/userpages/Account/Account.js
+++ src/userpages/Account/Account.js
@@ -9,16 +9,16 @@ const Account = (props) => {
     return (
         <div className="Account">
             <div className="AccountContainer">
-                <div className="row verticalCenter">
+                <div className="Accountrow verticalCenter">
                     <div className="PointsDetail">
-                        <div className="center"><h5>Points Detail</h5></div>
-                        <div className="row" style={{height: "50px"}}><h6 style={{width: "75%"}}>Current Balance: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
-                        <div className="row" style={{height: "50px"}}><h6 style={{width: "75%"}}>Total Earned Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
-                        <div className="row" style={{height: "50px"}}><h6 style={{width: "75%"}}>Total Withdrawn Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
-                        <div className="row" style={{height: "50px"}}><h6 style={{width: "75%"}}>Withdrawable Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
+                        <div className="AccountCenter"><h5>Points Detail</h5></div>
+                        <div className="Accountrow" style={{height: "50px"}}><h6 style={{width: "75%"}}>Current Balance: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
+                        <div className="Accountrow" style={{height: "50px"}}><h6 style={{width: "75%"}}>Total Earned Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
+                        <div className="Accountrow" style={{height: "50px"}}><h6 style={{width: "75%"}}>Total Withdrawn Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
+                        <div className="Accountrow" style={{height: "50px"}}><h6 style={{width: "75%"}}>Withdrawable Points: </h6><div style={{height: "50px"}}><h6>{EarnedPoints}</h6></div></div>
                     </div>
                 </div>
-                <div className="row verticalCenter"><div className="PointsDetail"></div></div>
+                <div className="Account verticalCenter"><div className="PointsDetail"></div></div>
             </div>
         </div>
     );
